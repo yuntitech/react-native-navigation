b98d60b496cecf8159ac82d624f6b460f6da8649
wenshe <1055328305@qq.com>
2019-07-09 15:27:39 +0800

    feat: navigation event 触发时, 发送通知传出当前 screen 的 props

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,22 +1,37 @@
 /*eslint-disable*/
-import React from 'react';
-import {AppRegistry} from 'react-native';
-import platformSpecific from './deprecated/platformSpecificDeprecated';
-import {Screen} from './Screen';
+import React from "react";
+import { AppRegistry, View, Text } from "react-native";
+import platformSpecific from "./deprecated/platformSpecificDeprecated";
+import { Screen } from "./Screen";
 
-import PropRegistry from './PropRegistry';
+import PropRegistry from "./PropRegistry";
 
 const registeredScreens = {};
 const _allNavigatorEventHandlers = {};
+const EVENT_NOTIFICATION = "EVENT_NOTIFICATION";
 
 function registerScreen(screenID, generator) {
   registeredScreens[screenID] = generator;
   AppRegistry.registerComponent(screenID, generator);
 }
 
-function registerComponent(screenID, generator, store = undefined, Provider = undefined, options = {}) {
+function registerComponent(
+  screenID,
+  generator,
+  store = undefined,
+  Provider = undefined,
+  options = {},
+  floatingView: ?any = null
+) {
   if (store && Provider) {
-    return _registerComponentRedux(screenID, generator, store, Provider, options);
+    return _registerComponentRedux(
+      screenID,
+      generator,
+      store,
+      Provider,
+      options,
+      floatingView
+    );
   } else {
     return _registerComponentNoRedux(screenID, generator);
   }
@@ -26,7 +41,9 @@ function _registerComponentNoRedux(screenID, generator) {
   const generatorWrapper = function() {
     const InternalComponent = generator();
     if (!InternalComponent) {
-      console.error(`Navigation: ${screenID} registration result is 'undefined'`);
+      console.error(
+        `Navigation: ${screenID} registration result is 'undefined'`
+      );
     }
 
     return class extends Screen {
@@ -36,19 +53,40 @@ function _registerComponentNoRedux(screenID, generator) {
       constructor(props) {
         super(props);
         this.state = {
-          internalProps: {...props, ...PropRegistry.load(props.screenInstanceID || props.passPropsKey)}
-        }
+          internalProps: {
+            ...props,
+            ...PropRegistry.load(props.screenInstanceID || props.passPropsKey)
+          }
+        };
+        this.navigator.addOnNavigatorEvent(event => {
+          DeviceEventEmitter.emit(EVENT_NOTIFICATION, {
+            event: event,
+            screenInstanceID:
+              this.props.screenInstanceID || this.props.passPropsKey,
+            screenID: screenID,
+            props: this.state.internalProps
+          });
+        });
       }
 
       componentWillReceiveProps(nextProps) {
         this.setState({
-          internalProps: {...PropRegistry.load(this.props.screenInstanceID || this.props.passPropsKey), ...nextProps}
-        })
+          internalProps: {
+            ...PropRegistry.load(
+              this.props.screenInstanceID || this.props.passPropsKey
+            ),
+            ...nextProps
+          }
+        });
       }
 
       render() {
         return (
-          <InternalComponent testID={screenID} navigator={this.navigator} {...this.state.internalProps} />
+          <InternalComponent
+            testID={screenID}
+            navigator={this.navigator}
+            {...this.state.internalProps}
+          />
         );
       }
     };
@@ -57,9 +95,17 @@ function _registerComponentNoRedux(screenID, generator) {
   return generatorWrapper;
 }
 
-function _registerComponentRedux(screenID, generator, store, Provider, options) {
+function _registerComponentRedux(
+  screenID,
+  generator,
+  store,
+  Provider,
+  options,
+  floatingView: ?any = null
+) {
   const generatorWrapper = function() {
     const InternalComponent = generator();
+    const AudioPlayFloatingView = floatingView ? floatingView() : null;
     return class extends Screen {
       static navigatorStyle = InternalComponent.navigatorStyle || {};
       static navigatorButtons = InternalComponent.navigatorButtons || {};
@@ -67,20 +113,59 @@ function _registerComponentRedux(screenID, generator, store, Provider, options) 
       constructor(props) {
         super(props);
         this.state = {
-          internalProps: {...props, ...PropRegistry.load(props.screenInstanceID || props.passPropsKey)}
-        }
+          internalProps: {
+            ...props,
+            ...PropRegistry.load(props.screenInstanceID || props.passPropsKey)
+          }
+        };
+        this.navigator.addOnNavigatorEvent(event => {
+          DeviceEventEmitter.emit(EVENT_NOTIFICATION, {
+            event: event,
+            screenInstanceID:
+              this.props.screenInstanceID || this.props.passPropsKey,
+            screenID: screenID,
+            props: this.state.internalProps
+          });
+        });
       }
 
       componentWillReceiveProps(nextProps) {
         this.setState({
-          internalProps: {...PropRegistry.load(this.props.screenInstanceID || this.props.passPropsKey), ...nextProps}
-        })
+          internalProps: {
+            ...PropRegistry.load(
+              this.props.screenInstanceID || this.props.passPropsKey
+            ),
+            ...nextProps
+          }
+        });
       }
 
       render() {
         return (
           <Provider store={store} {...options}>
-            <InternalComponent testID={screenID} navigator={this.navigator} {...this.state.internalProps} />
+            {floatingView ? (
+              <View style={{ flex: 1 }}>
+                <InternalComponent
+                  testID={screenID}
+                  navigator={this.navigator}
+                  {...this.state.internalProps}
+                />
+                <AudioPlayFloatingView
+                  navigator={this.navigator}
+                  screenID={screenID}
+                  screenInstanceID={this.props.screenInstanceID}
+                  customTitleBar={
+                    this.state.internalProps.enableCustomLargeTitle
+                  }
+                />
+              </View>
+            ) : (
+              <InternalComponent
+                testID={screenID}
+                navigator={this.navigator}
+                {...this.state.internalProps}
+              />
+            )}
           </Provider>
         );
       }
@@ -93,7 +178,9 @@ function _registerComponentRedux(screenID, generator, store, Provider, options) 
 function getRegisteredScreen(screenID) {
   const generator = registeredScreens[screenID];
   if (!generator) {
-    console.error(`Navigation.getRegisteredScreen: ${screenID} used but not yet registered`);
+    console.error(
+      `Navigation.getRegisteredScreen: ${screenID} used but not yet registered`
+    );
     return undefined;
   }
   return generator();
@@ -134,7 +221,7 @@ function dismissInAppNotification(params = {}) {
 async function startTabBasedApp(params) {
   try {
     return await platformSpecific.startTabBasedApp(params);
-  } catch(e) {
+  } catch (e) {
     console.error(`Error while starting app: ${e}`);
   }
 }
@@ -142,7 +229,7 @@ async function startTabBasedApp(params) {
 async function startSingleScreenApp(params) {
   try {
     return await platformSpecific.startSingleScreenApp(params);
-  } catch(e) {
+  } catch (e) {
     console.error(`Error while starting app: ${e}`);
   }
 }
@@ -161,7 +248,7 @@ function handleDeepLink(params = {}) {
   if (!link) return;
 
   const event = {
-    type: 'DeepLink',
+    type: "DeepLink",
     link,
     ...(payload ? { payload } : {})
   };
@@ -205,5 +292,6 @@ export default {
   handleDeepLink: handleDeepLink,
   isAppLaunched: isAppLaunched,
   isRootLaunched: isRootLaunched,
-  getLaunchArgs
+  getLaunchArgs: getLaunchArgs,
+  eventNotification: EVENT_NOTIFICATION
 };
