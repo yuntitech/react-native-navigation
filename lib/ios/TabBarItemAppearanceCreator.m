686bab13d46b57ffe99ed8220bc2d6f094dc063f
leejunhui <leejunhui2015@gmail.com>
2020-10-20 11:45:51 +0800

    fix: 修复 iPadOS 14 下 TabBar 文字显示问题

--- lib/ios/TabBarItemAppearanceCreator.m
+++ lib/ios/TabBarItemAppearanceCreator.m
@@ -8,18 +8,32 @@
     return tabBarItem;
 }
 
+// TODO: leejunhui 修复 iPadOS 14 下 tabbar 文字被截断的问题
 + (void)setTitleAttributes:(UITabBarItem *)tabItem titleAttributes:(NSDictionary *)titleAttributes {
     [super setTitleAttributes:tabItem titleAttributes:titleAttributes];
     tabItem.standardAppearance.stackedLayoutAppearance.normal.titleTextAttributes = titleAttributes;
     tabItem.standardAppearance.compactInlineLayoutAppearance.normal.titleTextAttributes = titleAttributes;
     tabItem.standardAppearance.inlineLayoutAppearance.normal.titleTextAttributes = titleAttributes;
+    if (@available(iOS 14.0, *)) {
+      UIColor *color = [titleAttributes valueForKey:@"NSColor"];
+      if (color) {
+        tabItem.standardAppearance.inlineLayoutAppearance.normal.titleTextAttributes = @{NSForegroundColorAttributeName: color};
+      }
+    }
 }
 
+// TODO: leejunhui 修复 iPadOS 14 下 tabbar 文字被截断的问题
 + (void)setSelectedTitleAttributes:(UITabBarItem *)tabItem selectedTitleAttributes:(NSDictionary *)selectedTitleAttributes {
     [super setSelectedTitleAttributes:tabItem selectedTitleAttributes:selectedTitleAttributes];
     tabItem.standardAppearance.stackedLayoutAppearance.selected.titleTextAttributes = selectedTitleAttributes;
     tabItem.standardAppearance.compactInlineLayoutAppearance.selected.titleTextAttributes = selectedTitleAttributes;
     tabItem.standardAppearance.inlineLayoutAppearance.selected.titleTextAttributes = selectedTitleAttributes;
+    if (@available(iOS 14.0, *)) {
+      UIColor *color = [selectedTitleAttributes valueForKey:@"NSColor"];
+      if (color) {
+        tabItem.standardAppearance.inlineLayoutAppearance.selected.titleTextAttributes = @{NSForegroundColorAttributeName: color};
+      }
+    }
 }
 
 @end
